d9dcb8314abddcb79ff7c3f70470fd01bccc1ba3
Mark <BurningCircle@yandex.ru>
2020-04-21 21:54:57 +0300

    feat: added color pickers to sidebar

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,6 +1,24 @@
-import React from "react"
+import React, { useContext } from "react"
+import {BaseButton} from '@components'
+import { Context } from "@/state/Context.js"
+import { ColorPick } from "./_ColorPick"
 import "./index.sass"
 
-export const Sidebar = () => (
-  <div className="sidebar"></div>
-)
+export const Sidebar = () => {
+  const {localization, colorPickers, addColorPicker} = useContext(Context)
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar__title">
+        {localization.current === "RU" ? "Параметры цвета" : "Color parameters"}
+      </div>
+      <span className="sidebar__splitter bold"></span>
+      <div className="sidebar__color-pickers">
+        {colorPickers.map((colorPick) => (
+          <ColorPick key={colorPick.id} {...colorPick} />
+        ))}
+      </div>
+        <BaseButton onClick={addColorPicker}>{localization.current === 'RU' ? 'Добавить цвет' : 'Add color'}</BaseButton>
+    </div>
+  )
+}
