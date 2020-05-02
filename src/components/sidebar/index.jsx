ab696d908e25845f1becf23a51257e8fb3e4c067
Mark <BurningCircle@yandex.ru>
2020-05-02 20:31:50 +0300

    fix: fixed style issues

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -4,19 +4,21 @@ import { Context } from "@/state/Context.js"
 import { ColorPick } from "./_ColorPick"
 import "./index.sass"
 
+const Header = ({ localization }) => (
+  <div className="sidebar__header">
+    <div className="sidebar__title">
+      {localization.current === "RU" ? "Параметры цвета" : "Color parameters"}
+    </div>
+    <span className="sidebar__splitter bold"></span>
+  </div>
+)
+
 export const Sidebar = () => {
   const { localization, colorPickers, addColorPicker } = useContext(Context)
 
   return (
     <div className="sidebar">
-      <div className="sidebar__header">
-        <div className="sidebar__title">
-          {localization.current === "RU"
-            ? "Параметры цвета"
-            : "Color parameters"}
-        </div>
-        <span className="sidebar__splitter bold"></span>
-      </div>
+      <Header localization={localization} />
       <div className="sidebar__color-pickers">
         <div className="sidebar__color-pickers__wrapper">
           {colorPickers.map((colorPick) => (
@@ -24,9 +26,11 @@ export const Sidebar = () => {
           ))}
         </div>
       </div>
-      <BaseButton onClick={addColorPicker}>
-        {localization.current === "RU" ? "Добавить цвет" : "Add color"}
-      </BaseButton>
+      <div className="sidebar__button">
+        <BaseButton onClick={addColorPicker}>
+          {localization.current === "RU" ? "Добавить цвет" : "Add color"}
+        </BaseButton>
+      </div>
     </div>
   )
 }
